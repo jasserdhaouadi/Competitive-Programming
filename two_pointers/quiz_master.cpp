6e182395dd498939c5d60ca4f1ffbdbb26942912
jasserdhaouadi <jasser.dhaouadi@rwth-aachen.de>
2025-08-05 21:25:24 +0200

    sync

--- two_pointers/quiz_master.cpp
+++ two_pointers/quiz_master.cpp
@@ -11,55 +11,50 @@ void dbg(int* x){
 
 const int INF = 1e9;
 
-void compute(bool dir, int x, int m, vector<int>& freq, int& cnt){
-    for(int i=1; i*i<=x && i<=m; i++){
-        if(x % i == 0){
-            if(dir){
-                freq[i]++;
-                freq[i] == 1 ? cnt++ : cnt;
-                if(x / i <= m){
-                    freq[x / i]++;
-                    freq[x / i] == 1 ? cnt++ : cnt;
-                }
-            }else{
-                freq[i]--;
-                freq[i] == 0 ? cnt-- : cnt;
-                if(x / i <= m){
-                    freq[x / i]--;
-                    freq[x / i] == 0 ? cnt-- : cnt;
-                }
-            }
-        }
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    vector<vector<int>> fact(1e5+1);
+    for(int i=1; i<=1e5; i++){
+        for(int j=i; j<=1e5; j+=i){
+            fact[j].push_back(i);
+        }
+    }
     int t;
     cin >> t;
     while(t--){
         int n, m;
         cin >> n >> m;
         vector<int> a(n);
+        vector<int> freq(m+1, 0);
         for(int& i : a)
             cin >> i;
         sort(a.begin(), a.end());
-
-        int ans = INF;
-        int l = 0;
-        vector<int> freq(m + 1, 0);
+        int ans = 1e9 + 5;
         int cnt = 0;
+        int l = 0;
         for(int r=0; r<n; r++){
-            compute(true, a[r], m, freq, cnt);
+            for(int& i : fact[a[r]]){
+                if(i > m)
+                    continue;
+                if(++freq[i] == 1)
+                    cnt++;
+            }
             while(cnt == m){
                 ans = min(ans, a[r] - a[l]);
-                compute(false, a[l], m, freq, cnt);
+                for(int& i : fact[a[l]]){
+                    if(i > m)
+                        continue;
+                    if(--freq[i] == 0)
+                        cnt--;
+                }
                 l++;
             }
         }
-        ans == INF ? cout << -1 : cout << ans;
-        cout << '\n';
+        if(ans == 1e9 + 5)
+            cout << "-1\n";
+        else 
+            cout << ans << '\n';
     }
     return 0;
 }
